02d1091ec6519cd2477373fa80ae6d5730d6edac
Jey-the-priest <jeromekendricks7@gmail.com>
2022-09-08 15:19:40 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-/** 
-* main - put the string in the put function
+/**
+* main - print the string in the main function
 *
 * Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle
