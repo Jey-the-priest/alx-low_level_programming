5ea6015a1ae4a63cc779f6392cb172e10fa0554a
Jey-the-priest <jeromekendricks7@gmail.com>
2022-09-16 15:14:59 -0700

    print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,10 +1,8 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
-* print_number - Prints a number.
-* @n: The number to print.
-* Description: Can only use _putchar to print.
+* print_number - Prints a number
+* @n: The number to print
 */
 
 void print_number(int n)
@@ -13,12 +11,12 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		num = -num;
 	}
 	if (num > 9)
 	{
 		print_number(num / 10);
 	}
-	putchar(num % 10 + '0');
+	_putchar(num % 10 + '0');
 }
