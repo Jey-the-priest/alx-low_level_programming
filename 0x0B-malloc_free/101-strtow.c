c2a71c2c11a9ec5bb73ce22993521c7c8d3e4b6b
Jey-the-priest <jeromekendricks7@gmail.com>
2022-10-05 05:54:27 -0700

    task 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,71 +1,52 @@
 #include "main.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
- * ch_free_grid - frees a 2 dimensional array.
- * @grid: multidimensional array of char.
- * @height: height of the array.
- *
- * Return: no return.
+ * argstostr - a function that concatenates all the arguments of your program
+ * @ac: the number of arguments to the program
+ * @av: the arguments to the program
+ * Return: returns a pointer to the concatenated string
  */
-
-void ch_free_grid(char **grid, unsigned int height)
+char *argstostr(int ac, char **av)
 {
-	if (grid != NULL && height != 0)
-	{
-		for (; height > 0; height--)
-			free(grid[height]);
-		free(grid[height]);
-		free(grid);
-	}
-}
+	int i, j, k;
+	char *str;
+	int c = 0;
 
-/**
- * strtow - splites a string into words.
- * @str: string.
- *
- * Return: pointer of an array of integers.
- */
-
-char **strtow(char *str)
-{
-	char **aout;
-	unsigned int c, height, i, j, a1;
-
-	if (str == NULL || *str == '\0')
+	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (c = height = 0; str[c] != '\0'; c++)
+	/* Counts the length of every character in the argument */
+	for (i = 0; i < ac; i++)
 	{
-		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
-			height++;
-		aout = malloc((height + 1) * sizeof(char *));
-		if (aout == NULL || height == 0)
+		for (j = 0; 1 ; j++)
 		{
-			free(aout);
-			return (NULL);
+			c++;
+			if (av[i][j] == '\0')
+				break;
 		}
-		for (i = a1 = 0; i < height; i++)
-		{
-			for (c = a1; str[c] != '\0'; c++)
-			{
-				if (str[c] == ' ')
-					a1++;
-				if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
-				{
-					aout[i] = malloc((c - a1 + 2) * sizeof(char));
-					if (aout[i] == NULL)
-					{
-						ch_free_grid(aout, i);
-						return (NULL);
-					}
-						break;
-				}
-			}
-			for (j = 0; a1 <= c; a1++, j++)
-	 			aout[i][j] = str[a1];
-			aout[i][j] = '\0';
-		}
-		aout[i] = NULL;
-		return (aout);
 	}
+	c += 1;
+	str = (char *) malloc(c);
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+	/* Concatenates every argument to the string str */
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; 1 ; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+
+			if (av[i][j] == '\0')
+			{
+				str[k - 1] = '\n';
+				break;
+			}
+		}
+	}
+	str[k] = '\0';
+
+	return (str);
 }
